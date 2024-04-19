9f2d0794a85f8f1cf2a5b3a879f2a9b4922b38c0
DucAnh <anhnguyen2k373@gmail.com>
2024-04-19 08:11:33 +0700

    Case: Number is not a prime number

--- src/main/java/com/example/learn/PrimeCheckerTest.java
+++ src/main/java/com/example/learn/PrimeCheckerTest.java
@@ -19,4 +19,23 @@ public class PrimeCheckerTest {
         // Thêm các trường hợp khác cho số nguyên tố lớn
     }
 
+    @Test
+    public void testIsPrime_WithNonPrimeNumber_ShouldReturnFalse() {
+        assertFalse(PrimeChecker.isPrime(1));
+        assertFalse(PrimeChecker.isPrime(4));
+        assertFalse(PrimeChecker.isPrime(6));
+        assertFalse(PrimeChecker.isPrime(8));
+        assertFalse(PrimeChecker.isPrime(9));
+        assertFalse(PrimeChecker.isPrime(10));
+        assertFalse(PrimeChecker.isPrime(12));
+        assertFalse(PrimeChecker.isPrime(14));
+        assertFalse(PrimeChecker.isPrime(15));
+        assertFalse(PrimeChecker.isPrime(16));
+        assertFalse(PrimeChecker.isPrime(18));
+        assertFalse(PrimeChecker.isPrime(20));
+        assertFalse(PrimeChecker.isPrime(21));
+        assertFalse(PrimeChecker.isPrime(22));
+        assertFalse(PrimeChecker.isPrime(26));
+        // Thêm các trường hợp khác cho số không phải là số nguyên tố
+    }
 }
